d935048326100bc013791ebf68f823bce19ff3c7
2acholsk1 <p.zacholski@wp.pl>
2024-10-28 10:32:11 +0000

    Adding CD/CI tools
    flake8; pre-commit; pylint; tests folder

--- src/demo/demo.py
+++ src/demo/demo.py
@@ -1,24 +1,22 @@
-import cv2
+from concurrent.futures import ThreadPoolExecutor
+
 import cv2 as cv
 import numpy as np
 import skimage.feature
 from numpy.random import random
-from scipy.signal import resample
-import matplotlib.pyplot as plt
-from concurrent.futures import ThreadPoolExecutor
 
-map = cv.imread('data/demo/gis.png')
+map_picture = cv.imread('data/demo/gis.png')
 # template = cv.imread('template_3.jpg')
 
 
 def systematic_resample(weights):
-    N = len(weights)
+    length = len(weights)
     # make N subdivisions, and choose positions with a consistent random offset
-    positions = (random() + np.arange(N)) / N
-    indexes = np.zeros(N, 'i')
+    positions = (random() + np.arange(length)) / length
+    indexes = np.zeros(length, 'i')
     cumulative_sum = np.cumsum(weights)
     i, j = 0, 0
-    while i < N:
+    while i < length:
         if positions[i] < cumulative_sum[j]:
             indexes[i] = j
             i += 1
@@ -28,7 +26,7 @@ def systematic_resample(weights):
 
 
 def generate_uav_trajectory(sequence_length, patch_size):
-    height, width, _ = map.shape
+    height, width, _ = map_picture.shape
     start_point = np.random.randint(0 + int(patch_size / 2) + 1, height - int(patch_size / 2) - 1)
     end_point = np.random.randint(0 + int(patch_size / 2) + 1, height - int(patch_size / 2) - 1)
     coord_heights = np.linspace(start_point, end_point, sequence_length)
@@ -40,7 +38,7 @@ def generate_uav_trajectory(sequence_length, patch_size):
 
 
 def initialize_particles(particle_number, patch_size):
-    height, width, _ = map.shape
+    height, width, _ = map_picture.shape
     particle_init_y = np.random.randint(0 + int(patch_size / 2) + 1, height - int(patch_size / 2) - 1,
                                         size=particle_number)
     particle_init_x = np.random.randint(0 + int(patch_size / 2) + 1, width - int(patch_size / 2) - 1,
@@ -51,8 +49,8 @@ def initialize_particles(particle_number, patch_size):
     return np.asarray(particle_init)
 
 
-def initialize_particles_at_loc(particle_number, patch_size, loc):
-    height, width, _ = map.shape
+def initialize_particles_at_loc(particle_number, loc):
+    # height, width, _ = map.shape
     particle_init_x = np.random_randint(loc[1] - 100, loc[1] + 100, size=particle_number)
     particle_init_y = np.random_randint(loc[0] - 100, loc[0] + 100, size=particle_number)
 
@@ -62,12 +60,12 @@ def initialize_particles_at_loc(particle_number, patch_size, loc):
     return np.asarray(particle_init)
 
 
-def collect_particle_desriptors(map, coordinate_list, patch_size):
+def collect_particle_desriptors(map_picture, coordinate_list, patch_size):
     descriptors = []
-    for i in range(len(coordinate_list)):
+    for i, _ in enumerate(coordinate_list):
         x = coordinate_list[i][0]
         y = coordinate_list[i][1]
-        descriptors.append(map[y - int(patch_size / 2):y + int(patch_size / 2) + 1,
+        descriptors.append(map_picture[y - int(patch_size / 2):y + int(patch_size / 2) + 1,
                            x - int(patch_size / 2):x + int(patch_size / 2) + 1])
     return descriptors
 
@@ -98,49 +96,51 @@ def match_particles_against_template(particle_descriptors, template):
     return scores_norm
 
 
-def move_particles(indices_after_resampling, particle_coordinates, map, template_size, move_model):
+def move_particles(indices_after_resampling, particle_coordinates, map_picture, template_size, move_model):
     new_particles = np.empty_like(particle_coordinates)
-    for i in range(len(indices_after_resampling)):
+    for i, _ in enumerate(indices_after_resampling):
         new_particles[i] = particle_coordinates[indices_after_resampling[i]]
     movements = np.random.randint(-20, 20, size=new_particles.shape)
     new_particles = new_particles + movements + move_model
     new_particles[:, 0] = np.clip(new_particles[:, 0], 0 + int(template_size / 2) + 1,
-                                  map.shape[1] - int(template_size / 2) - 1)
+                                  map_picture.shape[1] - int(template_size / 2) - 1)
     new_particles[:, 1] = np.clip(new_particles[:, 1], 0 + int(template_size / 2) + 1,
-                                  map.shape[0] - int(template_size / 2) - 1)
+                                  map_picture.shape[0] - int(template_size / 2) - 1)
     return new_particles
 
 
-def get_patch_at_coords(point, patch_size, map):
+def get_patch_at_coords(point, patch_size, map_picture):
     x = point[0]
     y = point[1]
-    patch = map[y - int(patch_size / 2):y + int(patch_size / 2) + 1,
-            x - int(patch_size / 2):x + int(patch_size / 2) + 1]
+    patch = map_picture[
+        y - int(patch_size / 2):y + int(patch_size / 2) + 1,
+        x - int(patch_size / 2):x + int(patch_size / 2) + 1
+        ]
     return patch
 
 
 def match_patches(candidate, template):
     # this is done in an extremely dumb way (repeatedly computing template descriptor) and needs ti be changes
-    hist_candidate_b = cv2.calcHist([candidate], [0], None, [16], [0, 256])
+    hist_candidate_b = cv.calcHist([candidate], [0], None, [16], [0, 256])
     hist_candidate_b = hist_candidate_b / hist_candidate_b.sum()
-    hist_candidate_g = cv2.calcHist([candidate], [1], None, [16], [0, 256])
+    hist_candidate_g = cv.calcHist([candidate], [1], None, [16], [0, 256])
     hist_candidate_g = hist_candidate_g / hist_candidate_g.sum()
-    hist_candidate_r = cv2.calcHist([candidate], [2], None, [16], [0, 256])
+    hist_candidate_r = cv.calcHist([candidate], [2], None, [16], [0, 256])
     hist_candidate_r = hist_candidate_r / hist_candidate_r.sum()
-    hist_template_b = cv2.calcHist([template], [0], None, [16], [0, 256])
+    hist_template_b = cv.calcHist([template], [0], None, [16], [0, 256])
     hist_template_b = hist_template_b / hist_template_b.sum()
-    hist_template_g = cv2.calcHist([template], [1], None, [16], [0, 256])
+    hist_template_g = cv.calcHist([template], [1], None, [16], [0, 256])
     hist_template_g = hist_template_g / hist_template_g.sum()
-    hist_template_r = cv2.calcHist([template], [2], None, [16], [0, 256])
+    hist_template_r = cv.calcHist([template], [2], None, [16], [0, 256])
     hist_template_r = hist_template_r / hist_template_r.sum()
 
-    candidate_lbp = skimage.feature.local_binary_pattern(cv2.cvtColor(candidate, cv2.COLOR_BGR2GRAY), 8, 1,
+    candidate_lbp = skimage.feature.local_binary_pattern(cv.cvtColor(candidate, cv.COLOR_BGR2GRAY), 8, 1,
                                                          method='nri_uniform')
-    template_lbp = skimage.feature.local_binary_pattern(cv2.cvtColor(template, cv2.COLOR_BGR2GRAY), 8, 1,
+    template_lbp = skimage.feature.local_binary_pattern(cv.cvtColor(template, cv.COLOR_BGR2GRAY), 8, 1,
                                                         method='nri_uniform')
-    hist_candidate_lbp = cv2.calcHist([candidate_lbp.astype(np.uint8)], [0], None, [32], [0, 31])
+    hist_candidate_lbp = cv.calcHist([candidate_lbp.astype(np.uint8)], [0], None, [32], [0, 31])
     hist_candidate_lbp = hist_candidate_lbp / hist_candidate_lbp.sum()
-    hist_template_lbp = cv2.calcHist([template_lbp.astype(np.uint8)], [0], None, [32], [0, 31])
+    hist_template_lbp = cv.calcHist([template_lbp.astype(np.uint8)], [0], None, [32], [0, 31])
     hist_template_lbp = hist_template_lbp / hist_template_lbp.sum()
 
     candidate_vector = np.vstack((hist_candidate_b, hist_candidate_g, hist_candidate_r, hist_candidate_lbp))
@@ -152,41 +152,47 @@ def match_patches(candidate, template):
     return result
 
 
-# sample_1 = cv2.imread('template.jpg')
-# sample_2 = cv2.imread('template_3.jpg')
+# sample_1 = cv.imread('template.jpg')
+# sample_2 = cv.imread('template_3.jpg')
 # result_comp = match_patches(sample_1, sample_2)
 
 
-# TODO: check the order of operations in the PF for correctness
+# check the order of operations in the PF for correctness
 
-uav_loc = 0
-particle_number = 100
-template_size = 31
+UAV_LOC = 0
+PARTICLE_NUMBER = 100
+TEMPLATE_SIZE = 31
 
-trajectory = generate_uav_trajectory(500, template_size)
-particle_coordinates = initialize_particles(particle_number, template_size)
+trajectory = generate_uav_trajectory(500, TEMPLATE_SIZE)
+particle_coordinates = initialize_particles(PARTICLE_NUMBER, TEMPLATE_SIZE)
 cv.namedWindow("image", cv.WINDOW_NORMAL)
-cv.resizeWindow("image", int(map.shape[1] / 1.3), int(map.shape[0] / 1.3))
+cv.resizeWindow("image", int(map_picture.shape[1] / 1.3), int(map_picture.shape[0] / 1.3))
 
 move_model = [0, 0]
-a = 0
+KEY_CHAR = 0
 while True:
-    map_canvas = np.copy(map)
-    point = trajectory[uav_loc]
-    for i in range(particle_number):
+    map_canvas = np.copy(map_picture)
+    point = trajectory[UAV_LOC]
+    for i in range(PARTICLE_NUMBER):
         cv.circle(map_canvas, particle_coordinates[i], 2, (0, 255, 255), 2)
         cv.circle(map_canvas, particle_coordinates[i], 4, (0, 0, 0), 2)
     cv.circle(map_canvas, point - move_model, 10, (255, 255, 0), 5)
-    cv.resizeWindow("image", int(map.shape[1] / 1.5), int(map.shape[0] / 1.5))
+    cv.resizeWindow("image", int(map_picture.shape[1] / 1.5), int(map_picture.shape[0] / 1.5))
     cv.imshow('image', map_canvas)
-    while a < 5:
-        a = cv.waitKey(10)
-    cv2.waitKey(10)
-    uav_loc += 1
-    template = get_patch_at_coords(point, template_size, map)
-    particle_descriptors = collect_particle_desriptors(map, particle_coordinates, template_size)
+    while KEY_CHAR < 5:
+        KEY_CHAR = cv.waitKey(10)
+    cv.waitKey(10)
+    UAV_LOC += 1
+    template = get_patch_at_coords(point, TEMPLATE_SIZE, map_picture)
+    particle_descriptors = collect_particle_desriptors(map_picture, particle_coordinates, TEMPLATE_SIZE)
     results = match_particles_against_template(particle_descriptors, template)
     indices_after_resampling = systematic_resample(results)
-    move_model = trajectory[uav_loc] - trajectory[uav_loc - 1]
+    move_model = trajectory[UAV_LOC] - trajectory[UAV_LOC - 1]
     print(move_model)
-    particle_coordinates = move_particles(indices_after_resampling, particle_coordinates, map, template_size, move_model)
+    particle_coordinates = move_particles(
+        indices_after_resampling,
+        particle_coordinates,
+        map_picture,
+        TEMPLATE_SIZE,
+        move_model
+        )
